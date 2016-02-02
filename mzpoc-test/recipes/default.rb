c8c89e4dd129d1b53ce998d7b9f4b0e4f62992f4
imranteli@gmail.com <imranteli@gmail.com>
2016-02-02 15:17:15 +0530

    updated default recipe

--- mzpoc-test/recipes/default.rb
+++ mzpoc-test/recipes/default.rb
@@ -16,11 +16,3 @@ service 'mysqld' do
   action :start
 end
 
-cookbook_file '/etc/yum.repos.d/mongodb-org-3.2.repo' do
-  source 'mongodb-org-3.2.repo'
-  action :create
-end
-
-package ['lsof', 'wget', 'jdk', 'mongodb'] do
-  action :install
-end
