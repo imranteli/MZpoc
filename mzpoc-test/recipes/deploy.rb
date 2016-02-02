5a95ff5518dae93b1024f9967d5b53b5d8be0a7a
imranteli <imranteli@gmail.com>
2016-02-02 11:28:14 +0530

    deploy recipe added

---
+++ mzpoc-test/recipes/deploy.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: mzpoc-test
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+
+cookbook_file '/etc/yum.repos.d/mongodb-org-3.2.repo' do
+  source 'mongodb-org-3.2.repo'
+  action :create
+end
+
+package ['lsof', 'wget', 'jdk', 'mongodb'] do
+  action :install
+end
