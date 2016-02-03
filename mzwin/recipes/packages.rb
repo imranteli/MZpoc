6a36f5906b9ce6c2fbc7b9d4460491784f0dea7f
imranteli@gmail.com <imranteli@gmail.com>
2016-02-03 11:37:45 +0530

    windows update

--- mzwin/recipes/packages.rb
+++ mzwin/recipes/packages.rb
@@ -4,10 +4,10 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-#%w{ IIS-WebServerRole IIS-WebServer }.each do |feature|
-#  windows_feature feature do
-#    action :install
-#  end
-#end
+%w{ IIS-WebServerRole IIS-WebServer }.each do |feature|
+  windows_feature feature do
+    action :install
+  end
+end
 
 include_recipe "dotnetframework-master::default"
