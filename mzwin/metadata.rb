0bb845e6c2adbe932088f8a23523729873601543
imranteli@gmail.com <imranteli@gmail.com>
2016-02-03 12:06:44 +0530

    added windows cookbooks

--- mzwin/metadata.rb
+++ mzwin/metadata.rb
@@ -6,3 +6,7 @@ description 'Installs/Configures mzwin'
 long_description 'Installs/Configures mzwin'
 version '0.1.0'
 
+
+source "https://supermarket.chef.io"
+
+cookbook 'windows'
--- mzwin/recipes/packages.rb
+++ mzwin/recipes/packages.rb
@@ -4,10 +4,16 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-%w{ IIS-WebServerRole IIS-WebServer }.each do |feature|
-  windows_feature feature do
-    action :install
-  end
+#%w{ IIS-WebServerRole IIS-WebServer }.each do |feature|
+#  windows_feature feature do
+#    action :install
+#  end
+#end
+
+powershell_script 'Install IIS' do
+  code 'Add-WindowsFeature Web-Server'
+  guard_interpreter :powershell_script
+  not_if "(Get-WindowsFeature -Name Web-Server).Installed"
 end
 
 include_recipe "dotnetframework-master::default"
