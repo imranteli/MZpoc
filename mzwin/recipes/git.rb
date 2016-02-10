9758f46cb220be036377fdb4ebb6a8d9bd845173
imranteli@gmail.com <imranteli@gmail.com>
2016-02-10 15:16:09 +0530

    unzip code

--- mzwin/recipes/git.rb
+++ mzwin/recipes/git.rb
@@ -4,8 +4,18 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-git 'D:/MozartV2_POC' do
+
+directory 'D:/mzzipcode/' do
+  action :create
+end
+
+git 'D:/mzzipcode/' do
   repository 'https://github.com/imranteli/MZpoc.git'
   revision 'artifacts'
   action :sync
+end
+
+windows_zipfile 'D:/MozartV2_POCi/' do
+  source 'D:/mzzipcode/artifacts/file21.zip'
+  action :unzip
 end
