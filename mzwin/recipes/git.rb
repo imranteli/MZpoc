5bcd116a44ac00617a5ffd01bf1b57f1bfe266a1
imranteli@gmail.com <imranteli@gmail.com>
2016-02-10 16:15:55 +0530

    file close

--- mzwin/recipes/git.rb
+++ mzwin/recipes/git.rb
@@ -17,6 +17,7 @@ end
 
 aFile = File.new("D:/mzzipcode/artifacts/filename.txt", "r")
 zipname = aFile.read.strip
+aFile.close
 
 windows_zipfile 'D:/MozartV2_POC/' do
   source "D:/mzzipcode/artifacts/#{zipname}"
