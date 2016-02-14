fd0178bbea2fcfb99cf60b16f15cc3c912fd8251
imranteli@gmail.com <imranteli@gmail.com>
2016-02-14 19:09:24 +0530

    test 123

--- mzwin/recipes/git.rb
+++ mzwin/recipes/git.rb
@@ -4,11 +4,6 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-git_client 'default' do
-  action :install
-end
-
-
 
 directory 'D:/mzzipcode/' do
   action :create
---
+++ mzwin/recipes/packages.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: mzwin
+# Recipe:: git
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+git_client 'default' do
+  action :install
+end
+
